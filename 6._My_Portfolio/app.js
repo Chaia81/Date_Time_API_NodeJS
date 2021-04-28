f8dcbc3e9be6779c9081e1636648033e3772d988
Chaia81 <wagner_maria@hotmail.com>
2021-04-28 10:43:00 +0200

    Created frontpage with nav and footer. Added some styling to the page.

--- 6._My_Portfolio/app.js
+++ 6._My_Portfolio/app.js
@@ -1,14 +1,17 @@
 const express = require ("express");
 const app = express();
 
-// app.use(express.json());
-app.use(express.static('public'));
+// I use app.use(express.static("public")); to be able to read the css-file
+app.use(express.static("public"));
+app.use(express.json());
+app.use('/public/nav/', express.static('./public/nav/'));
 
 
 
-// Home
+
+/* -------------------- frontpage -------------------- */
 app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
+    res.sendFile(__dirname + "/public/frontpage/frontpage.html");
 });
 
 
@@ -22,9 +25,18 @@ app.get("/", (req, res) => {
 +--------------------------------------------------------------------------------------------------------------------------------------+
 */
 
+const server = app.listen(process.env.PORT || 8080, (error) => {
+    if (error) {
+        console.log(error);
+    }
+    console.log("Server is running on port", 8080);
+});
+
+/*
 app.listen(8080, (error) => {
     if (error) {
         console.log(error);
     }
     console.log("Server is running on port", 8080);
 });
+*/
